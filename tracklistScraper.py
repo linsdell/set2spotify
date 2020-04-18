bf61228dbe52e8a5c82f93e3981736ed1ac68a60
Jamie Linsdell <jamie.linsdell@gmail.com>
2020-04-18 16:07:25 -0400

    refactor: fix constant references

--- tracklistScraper.py
+++ tracklistScraper.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import sys
-import constant
 
 site= "https://www.1001tracklists.com/tracklist/1klq0dvt/elif-the-anjunadeep-edition-299-2020-04-16.html"
 hdr = {'User-Agent': 'Mozilla/5.0'}
@@ -19,7 +18,7 @@ trackList = []
 trackAvailable = []
 
 # Get all the track info rows in the table
-trackTable = soup.find_all(class_=constant.TRACK_INFO_DIV_CLASS)
+trackTable = soup.find_all(class_=TRACK_INFO_DIV_CLASS)
 # TODO: Make robust in that track name is not accessed as the first meta tag
 # Iterate through the table and get all the track names
 for track in trackTable:
@@ -27,11 +26,11 @@ for track in trackTable:
     trackList.append(trackName['content'])
 
 # Get all the media link rows in the table
-linksTable = soup.find_all(class_=constant.MEDIA_LINKS_DIV_CLASS)
+linksTable = soup.find_all(class_=MEDIA_LINKS_DIV_CLASS)
 # Print binary condition for the track having a spotify link
 for link in linksTable:
-    spotifyLink = link.find_next(class_=constant.SPOTIFY_LINK_ICON_CLASS)
-    trackAvailable.append(constant.MEDIA_LINKS_DIV_CLASS in spotifyLink['class'])
+    spotifyLink = link.find_next(class_=SPOTIFY_LINK_ICON_CLASS)
+    trackAvailable.append(MEDIA_LINKS_DIV_CLASS in spotifyLink['class'])
 
 if (len(trackList) != len(trackAvailable)):
     print("Length mismatch between track names and track availability")
