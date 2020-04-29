1897a0994b7f2c1421acca02ee339e8f92998294
Jamie Linsdell <jamie.linsdell@gmail.com>
2020-04-28 20:47:59 -0400

    Add logic to check if track is identified

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ username = 'jlinsdell'
 # TEST playlist ID
 playlist_id = '2dl4t2rD4lQ3PjpKKa27im'
 
-set_url = "https://www.1001tracklists.com/tracklist/pn9j9ck/luttrell-the-anjunadeep-edition-287-2020-01-16.html"
+set_url = "https://www.1001tracklists.com/tracklist/x0qf5b9/hot-since-82-roof-top-vibes-in-buenos-aires-argentina-2018-03-08.html"
 
 token = util.prompt_for_user_token(username,scope)
 
@@ -19,3 +19,5 @@ tracklist = tracklistScraper.getTracklist(set_url)
 trackIDs = spotifyComponent.getTrackIDsFromTracklist(token,tracklist)
 
 spotifyComponent.addTracksToPlaylistID(token,username,playlist_id,trackIDs)
+
+# TODO: Add description to playlist of the set names that are in it
--- tracklistScraper.py
+++ tracklistScraper.py
@@ -2,11 +2,7 @@ from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import sys
 
-site= "https://www.1001tracklists.com/tracklist/1klq0dvt/elif-the-anjunadeep-edition-299-2020-04-16.html"
-hdr = {'User-Agent': 'Mozilla/5.0'}
-req = Request(site,headers=hdr)
-page = urlopen(req)
-soup = BeautifulSoup(page,'html.parser')
+
 
 TRACK_INFO_DIV_CLASS = "tlToogleData"
 
@@ -17,7 +13,14 @@ SPOTIFY_LINK_ICON_CLASS = "fa-spotify"
 SPOTIFY_LINK_AVAILABLE_CLASS = "mediaAction"
 
 # TODO: Make robust in that track name is not accessed as the first meta tag
-def getTracklist():
+def getTracklist(trackListURL):
+    # Initalize parser and read HTML
+    site= trackListURL
+    hdr = {'User-Agent': 'Mozilla/5.0'}
+    req = Request(site,headers=hdr)
+    page = urlopen(req)
+    soup = BeautifulSoup(page,'html.parser')
+
     trackList = []
     trackAvailable = []
     finalTrackList = []
@@ -26,8 +29,11 @@ def getTracklist():
 
     # Iterate through the table and get all the track names
     for track in trackTable:
-        trackName = track.find_next("meta")
-        trackList.append(trackName['content'])
+        # Check if track has been identified. This deals with IDs
+        if (track.has_attr('itemprop')):
+            trackName = track.find_next("meta")
+            trackList.append(trackName['content'])
+            print(trackName['content'])
 
     # Get all the media link rows in the table
     linksTable = soup.find_all(class_=MEDIA_LINKS_DIV_CLASS)
@@ -38,6 +44,9 @@ def getTracklist():
 
     if (len(trackList) != len(trackAvailable)):
         print("Length mismatch between track names and track availability")
+        print(len(trackList),len(trackAvailable))
+        # for i in range(0, min(len(trackList),len(trackAvailable))):
+        #     print(trackList[i])
         sys.exit()
     for i in range(0,len(trackList)):
         if(trackAvailable[i]):
